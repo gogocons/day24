d87fe887d9b4df91916460e38bc4312b7a9a8afc
gogocons <gogocons@gmail.com>
2023-02-13 02:53:02 -0600

    images uploading, see TODOs

--- server/videogame-backend.ts
+++ server/videogame-backend.ts
@@ -2,16 +2,25 @@ import express from "express";
 import fs from "fs";
 import type { Videogame } from "./domain/videogame";
 import cors from "cors";
-import fileupload from "express-fileupload";
+import multer from "multer";
+const upload = multer({ dest: "uploads/" });
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// used for middleware to handle files
-app.use(fileupload());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.static('public'));
+app.use(express.static('uploads'));
+
+app.post("/cover", upload.single("cover-art"), function (req, res, next) {
+  console.log(req.file);
+  
+  // TODO: append the file suffix
+
+  // req.file is the `cover-art` file
+  // req.body will hold the text fields, if there were any
+});
 
 // at /videogames, I need to open that file, videogames.json, and then return that to the client.
 app.get("/videogames", function (req, res) {
@@ -22,6 +31,9 @@ app.get("/videogames", function (req, res) {
 
 app.post("/videogame", function (req, res) {
   console.log("CALLED POST VIDEOGAME ROUTE");
+
+  // TODO: get the latest image upload to add to the JSON for the game data
+
   // req.body has the incoming JSON data.
   const name = req.body.name;
   const platform = req.body.platform;
@@ -36,11 +48,13 @@ app.post("/videogame", function (req, res) {
     releaseYear: releaseYear,
     genre: genre,
     ratingAgency: ratingAgency,
-    goodGame: goodGame
+    goodGame: goodGame,
   };
 
   // read the file, add our videogame to the array, save the file.
-  const videogames = JSON.parse(fs.readFileSync("./videogames.json") as any as string);
+  const videogames = JSON.parse(
+    fs.readFileSync("./videogames.json") as any as string
+  );
   videogames.push(videogame);
   fs.writeFileSync("./videogames.json", JSON.stringify(videogames));
 
